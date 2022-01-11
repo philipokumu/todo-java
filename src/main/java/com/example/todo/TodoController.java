ff2facb0abce0811c56dace14aec8d7dd541f175
Philip Okumu <philipo.okumu@gmail.com>
2022-01-11 09:59:08 +0300

    Added post and get

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -1,10 +1,11 @@
 package com.example.todo;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,20 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
 
     @Autowired
-	// private TodoService todoService;
-    
+	private TodoService todoService;
+
     @RequestMapping("/todos")
     public List<Todo> getAllTodos() {
-        return Arrays.asList(
-            new Todo("1","Buy my food",true),
-            new Todo("2","Buy my internet",true)
-        );
+        return todoService.getTodos();
     }
 
 
-    // @RequestMapping("/todos/{id}")
-    // public String getTodo(@PathVariable String id) {
-    //     return List.of("todo1","todo2");
-    // }
+    @RequestMapping("/todos/{id}")
+    public Todo getTodo(@PathVariable String id) {
+        return todoService.getTodo(id);
+    }
+
+    @PostMapping("/todos")
+	public void addTodo(@RequestBody Todo todo) {
+		todoService.addTodo(todo);
+	}
     
 }
--- src/main/java/com/example/todo/TodoService.java
+++ src/main/java/com/example/todo/TodoService.java
@@ -1,17 +1,29 @@
 package com.example.todo;
 
-// import java.util.Arrays;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
-    // private List<Todo> todos =  Arrays.asList(
-    //     new Todo('1','Buy my food',true),
-    //     new Todo('1','Buy my food',true),
-    // )
+    private List<Todo> todos =  new ArrayList<>(Arrays.asList(
+        new Todo("1","Buy my food",true),
+        new Todo("2","Buy my chips",false)
+    ));
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public Todo getTodo(String id) {	
+		return todos.stream().filter(todo -> todo.getId().equals(id)).findFirst().get();
+	}
+
+    public void addTodo(Todo todo) {	
+		todos.add(todo);
+	}
     
 }
